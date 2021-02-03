3909af334a599fe8c537f76b4dfb982504dfc3a7
Dantolos <45291569+Dantolos@users.noreply.github.com>
2021-02-03 15:23:46 +0100

    1.0.66

--- Templates/events.php
+++ Templates/events.php
@@ -12,7 +12,7 @@ the_content();
 echo '<div class="se2-post-wall-categories container categorie-container" type="event">' . $Events->call_Events_Categories('year') . '</div>';
  
 //EVENT OUTPUT
-echo '<div class="se2-post-wall se2-events-wall container se-wall">' . $Events->call_Events_Wall( false, 'ASC','meta_value', 'date' ) . '</div>';
+echo '<div class="se2-post-wall se2-events-wall container se-wall">' . $Events->call_Events_Wall( false, 'ASC','meta_value', 'eckdaten', 'date', true ) . '</div>';
 
 get_footer(); 
 ?>
