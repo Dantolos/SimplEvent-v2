c41ad879477c97613eb149f599701c2e2a418a5f
Dantolos <45291569+Dantolos@users.noreply.github.com>
2021-02-04 17:04:34 +0100

    1.0.73

--- header.php
+++ header.php
@@ -104,7 +104,7 @@
 
 function theme_add_files() 
 {
-     $scriptversion = '1.0.33'; 
+     $scriptversion = '1.0.34'; 
     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.0.01' );
     wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style/dist/style.min.css', '', $scriptversion );
     
