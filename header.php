9853b55a88e43895d9c9fd345693f1fe0e284974
Dantolos <45291569+Dantolos@users.noreply.github.com>
2021-09-08 11:06:12 +0200

    1.1.87

--- header.php
+++ header.php
@@ -125,8 +125,8 @@
 
 function theme_add_files() 
 {
-     $scriptversion = '1.0.52'; 
-     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.0.16' );
+     $scriptversion = '1.0.53'; 
+     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.0.17' );
      wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style/build/style.css', '', $scriptversion );
      
      //3rd libraries
