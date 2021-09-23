3d045c68953605367c8b52300fcbf0c2204ba99f
Dantolos <aaron.giaimo@gmail.com>
2021-09-23 17:52:01 +0200

    1.2.12

--- theme/plugins/se2-cta/se2-cta.php
+++ theme/plugins/se2-cta/se2-cta.php
@@ -5,7 +5,7 @@ class se2_CTA{
 
     public function __construct(){
         if(get_option( 'se_cta_activ' ) === 'on'){
-            wp_enqueue_script( 'se2_cta_script', get_template_directory_uri() . '/theme/plugins/se2-cta/se2-cta.js', array('jquery'), '1.0.08', true );
+            wp_enqueue_script( 'se2_cta_script', get_template_directory_uri() . '/theme/plugins/se2-cta/se2-cta.js', array('jquery'), '1.0.09', true );
         }
     }
 
