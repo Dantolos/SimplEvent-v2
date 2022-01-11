bd507de54c5a80377268cc0edd67df2b3fc58ea5
Dantolos <45291569+Dantolos@users.noreply.github.com>
2022-01-11 15:03:46 +0100

    1.2.57

--- functions.php
+++ functions.php
@@ -91,7 +91,7 @@ add_filter('acf/settings/load_json', 'my_acf_json_load_point');
 function my_acf_json_load_point( $paths ) {
     unset($paths[0]);
     $paths[] = get_stylesheet_directory() . '/acf';
-    return $paths;
+    return $paths[0];
 }
 
 
