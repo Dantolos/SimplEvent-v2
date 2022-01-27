2a6472e4d5eab9325f35cb2359667c764c57ce35
Dantolos <45291569+Dantolos@users.noreply.github.com>
2022-01-27 19:21:00 +0100

    1.2.69

--- scripts/inc/anchor.js
+++ scripts/inc/anchor.js
@@ -1,15 +1,16 @@
 class se2_Anchor {
+
     constructor() {
         this.PARAM = location.search.split('anchor=')[1]
-        if(this.PARAM) {
+        if (this.PARAM) {
             this.PARAM = this.PARAM.split('&')[0]
         }
     }
 
-    goTO( targetID = false ) {
+    goTO(targetID = false) {
         var TARGET = (targetID) ? targetID : this.PARAM;
-        if( document.getElementById(TARGET) ){
-            document.getElementById(TARGET).scrollIntoView({ behavior: 'smooth', block: 'center'})
+        if (document.getElementById(TARGET)) {
+            document.getElementById(TARGET).scrollIntoView({ behavior: 'smooth', block: 'center' })
         }
     }
 
