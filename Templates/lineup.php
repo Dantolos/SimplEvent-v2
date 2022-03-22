13067988e63e5a6d1bb0ccdb08de3a1184fecb67
Dantolos <aaron.giaimo@gmail.com>
2022-03-22 17:53:24 +0100

    1.2.96

--- Templates/lineup.php
+++ Templates/lineup.php
@@ -28,21 +28,36 @@ the_content();
 
 echo $LineUp->cast_line_up_filter_section( get_the_ID() );
 
-$year = get_field( 'jahr', get_the_ID() );
+
+
+$year = get_field( 'visibility', get_the_ID() )['jahr'];
 if( isset( $_GET["j"] ) ){
      $yearTax = get_term_by( 'name', htmlspecialchars( $_GET["j"] ), 'jahr' );
      $year = $yearTax->term_id;
 }
+
+$event = 'all';
+if(get_field( 'visibility', get_the_ID() )['event']) {
+     $event = array();
+     foreach(get_field( 'visibility', get_the_ID() )['event'] as $selevent){
+          $event[$selevent['value']] = $selevent['label'];
+     }    
+}
+
+
 $cat = 'all';
 if(get_field( 'speaker_kategorie', get_the_ID() )) {
      $cat = get_field( 'speaker_kategorie', get_the_ID() );
 }
 
+
+
 $args = array(
      'view' => 'grid',
      'sort' => 'ASC', 
      'cat' => $cat,
-     'year' => $year
+     'year' => $year,
+     'event' => $event,
 );
 
 echo $LineUp->cast_line_up_overview( $args );
