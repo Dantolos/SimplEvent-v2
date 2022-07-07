09df24aebca97b6e10b2e4ad6dda07a33dc3a721
Dantolos <aaron.giaimo@gmail.com>
2022-07-07 16:06:39 +0200

    1.3.20
    Quicknav Mediacorner

--- header.php
+++ header.php
@@ -132,7 +132,7 @@
 
 function theme_add_files() 
 {
-     $scriptversion = '1.0.86'; 
+     $scriptversion = '1.0.88'; 
      wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.1.54' );
      wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style/build/style.css', '', $scriptversion );
      
