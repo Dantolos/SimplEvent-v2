274e7415ce35bf9b09269e11dff4072bed6d9915
Dantolos <aaron.giaimo@gmail.com>
2022-07-07 16:03:15 +0200

    1.3.19

--- header.php
+++ header.php
@@ -133,7 +133,7 @@
 function theme_add_files() 
 {
      $scriptversion = '1.0.86'; 
-     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.1.53' );
+     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.1.54' );
      wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style/build/style.css', '', $scriptversion );
      
      //3rd libraries
