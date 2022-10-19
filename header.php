8b14a89cb8e2f0c0ce82e4fcccaf922acdcbfb6b
Dantolos <aaron.giaimo@gmail.com>
2022-10-19 18:24:09 +0200

    1.3.52

--- header.php
+++ header.php
@@ -137,7 +137,7 @@
 function theme_add_files() 
 {
      $scriptversion = '1.0.98'; 
-     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.1.64' );
+     wp_enqueue_style( 'wp-style-css', get_template_directory_uri() . '/style.css', '', '1.1.65' );
      wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style/build/style.css', '', $scriptversion );
      
      //3rd libraries
--- inc/classes/people.class.php
+++ inc/classes/people.class.php
@@ -38,40 +38,40 @@ class People{
             }
           
             $this->output .= '<div class="se2-people-box">';
-            $this->output .= '<div class="se2-people-portrait" style="background-image:url(' . esc_url( get_field('foto', $person->ID) ) . ')"></div>';
-            $this->output .= '<div class="se2-people-content">';
+                $this->output .= '<div class="se2-people-portrait" style="background-image:url(' . esc_url( get_field('foto', $person->ID) ) . ')"></div>';
+                $this->output .= '<div class="se2-people-content">';
+                
+                    $name = ( get_field('vorname',  $person->ID) ) 
+                    ? 
+                        get_field('degree',  $person->ID) 
+                        . ' ' . get_field('vorname',  $person->ID) 
+                        . ' <b>' . get_field('nachname',  $person->ID) . '</b>'
+                    : 
+                        the_title();
+                    $this->output .= '<h2>' . $name . '</h2>';
+
+                    $firma = ( get_field('firma', $person->ID) ) ? ', ' . get_field('firma', $person->ID) : '';
+                    $this->output .= '<p>' . esc_attr( get_field('funktion', $person->ID) ) . $firma . '</p>';
+
+                    //Banchen
+                    $this->output .= '<p class="se2-people-branche">';
+                    if(get_the_terms($person->ID, 'branche')){
+                    foreach(get_the_terms($person->ID, 'branche') as $key => $branche ) {
+                            $spanstyle = ($key%2 == 0) ? 'opacity:1;' : 'opacity:.6;';
+                            $this->output .= ($key != 0) ? ' | ' : ' ';
+                            $this->output .= '<span style="' . $spanstyle . '">' . esc_attr($branche->name) . ' </span>';
+                        }
+                    }
+                    $this->output .= '</p>';
+
+                    //Social Media
+                    $socialmedia = new SocialMedia('#010101');
+                    
+                    if( get_field( 'social_media', $person->ID )["social_media"]  ){
+                        $this->output .= $socialmedia->castSocialMediaItem( get_field( 'social_media', $person->ID ) );
+                    }
             
-            $name = ( get_field('vorname',  $person->ID) ) 
-            ? 
-                 get_field('degree',  $person->ID) 
-                 . ' ' . get_field('vorname',  $person->ID) 
-                 . ' <b>' . get_field('nachname',  $person->ID) . '</b>'
-            : 
-                 the_title();
-            $this->output .= '<h2>' . $name . '</h2>';
-
-            $firma = ( get_field('firma', $person->ID) ) ? ', ' . get_field('firma', $person->ID) : '';
-            $this->output .= '<p>' . esc_attr( get_field('funktion', $person->ID) ) . $firma . '</p>';
-
-            //Banchen
-            $this->output .= '<p class="se2-people-branche">';
-            if(get_the_terms($person->ID, 'branche')){
-               foreach(get_the_terms($person->ID, 'branche') as $key => $branche ) {
-                    $spanstyle = ($key%2 == 0) ? 'opacity:1;' : 'opacity:.6;';
-                    $this->output .= ($key != 0) ? ' | ' : ' ';
-                    $this->output .= '<span style="' . $spanstyle . '">' . esc_attr($branche->name) . ' </span>';
-                }
-            }
-            $this->output .= '</p>';
-
-            //Social Media
-            $socialmedia = new SocialMedia('#010101');
-            
-            if( get_field( 'social_media', $person->ID )["social_media"]  ){
-               $this->output .= $socialmedia->castSocialMediaItem( get_field( 'social_media', $person->ID ) );
-            }
-         
-            $this->output .= '</div>';
+                $this->output .= '</div>';
             $this->output .= '</div>';
         }
         
