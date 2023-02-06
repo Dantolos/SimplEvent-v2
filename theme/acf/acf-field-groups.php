d662fa60704263bc7a929a9e74214c7e34ed3e9e
Corinne Baumann <corinne.baumann@opten.ch>
2023-02-06 09:08:38 +0100

    fix: show correct language programm

--- theme/acf/acf-field-groups.php
+++ theme/acf/acf-field-groups.php
@@ -7398,7 +7398,7 @@ acf_add_local_field_group(array(
 				'class' => '',
 				'id' => '',
 			),
-			'wpml_cf_preferences' => 2,
+			'wpml_cf_preferences' => 1,
 			'default_value' => '',
 			'placeholder' => '',
 			'prepend' => '',
